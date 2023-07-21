772392b6ef97a5223eeba5615f42dd29e28fa007
Shreyas Mhaske <97455512+shreyasmhaske@users.noreply.github.com>
2023-07-21 16:43:15 +0530

    add files

---
+++ app/src/main/java/com/example/change_app_theme/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.change_app_theme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
